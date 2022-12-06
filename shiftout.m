d925f3b9c43c56276717b5268f615b23cf01194a
oshima <66927117+nupenupe@users.noreply.github.com>
2022-12-06 16:52:44 +0900

    First commit

---
+++ shiftout.m
@@ -0,0 +1,18 @@
+function shiftout(obj,dataPin, clockPin,bit,val)
+%SHIFTOUT シフトレジスタに出力
+%   obj: マイコン
+%   dataPin:  dataPinのピン番号
+%   clockPin:  clockPinのピン番号
+%   bit: 送信ビット数
+%   val:シフトレジスタに送る値(2進数)
+
+for i = 1:bit
+    writeDigitalPin(obj,dataPin,bitand(bitshift(val,1-i),0b1));
+    writeDigitalPin(obj,clockPin,1);
+    writeDigitalPin(obj,clockPin,0);
+end
+    writeDigitalPin(obj,dataPin,0);
+
+
+end
+
